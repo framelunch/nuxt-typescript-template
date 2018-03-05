c38ce0e62388f630d25f6e998192ff587e0bb414
kaave <abe@framelunch.jp>
2018-03-05 22:46:34 +0900

    Initial commit

---
+++ components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <h2> </h2>
+    <img :src="'https://robohash.org/' + person.first_name + '_' + person.last_name" />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
+
+@Component({})
+export default class Card extends Vue {
+  @Prop() person;
+}
+</script>
+<style scoped>
+.card {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  padding: 1rem;
+  margin: 0.25rem;
+  border: 0.25rem solid gainsboro;
+}
+</style>
---
+++ store/index.ts
@@ -0,0 +1,16 @@
+export const state = () => ({
+  people: [],
+});
+
+export const mutations = {
+  setPeople(state, people) {
+    state.people = people;
+  },
+};
+
+export const actions = {
+  async nuxtServerInit({ commit }, { app }) {
+    const people = await app.$axios.$get('./random-data.json');
+    commit('setPeople', people.slice(0, 10));
+  },
+};
