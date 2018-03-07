ed8b476395be4df31d227cbd5e5135d65f77e366
kaave <abe@framelunch.jp>
2018-03-07 12:20:14 +0900

    [Common] robots.txt追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,53 +9,38 @@ const postcssUrl = require('postcss-url');
 const autoprefixer = require('autoprefixer');
 
 const argv = parseArgs(process.argv.slice(2), {
-  alias: {
-    H: 'hostname',
-    p: 'port'
-  },
+  alias: { H: 'hostname', p: 'port' },
   string: ['H'],
   unknown: parameter => false
 });
+const port = argv.port || process.env.PORT || process.env.npm_package_config_nuxt_port || '3000';
+const host = argv.hostname || process.env.HOST || process.env.npm_package_config_nuxt_host || 'localhost';
+const postcss = [
+  postcssImport(),
+  postcssCustomProperties(),
+  postcssCustomMedia(),
+  postcssNested(),
+  postcssColorHexAlpha(),
+  postcssFixes(),
+  postcssUrl(),
+  autoprefixer(),
+];
 
-const port =
-  argv.port ||
-  process.env.PORT ||
-  process.env.npm_package_config_nuxt_port ||
-  '3000';
-const host =
-  argv.hostname ||
-  process.env.HOST ||
-  process.env.npm_package_config_nuxt_host ||
-  'localhost';
 module.exports = {
   srcDir: 'src/',
   env: {
-    baseUrl:
-      process.env.BASE_URL ||
-      `http://${host}:${port}`
+    baseUrl: process.env.BASE_URL || `http://${host}:${port}`
   },
   head: {
     title: 'tt1',
     meta: [
       { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content:
-          'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'Nuxt.js project'
-      }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
     ],
     link: [
-      {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
   /*
   ** Customize the progress-bar color
@@ -73,21 +58,10 @@ module.exports = {
   ** Build configuration
   */
   css: ['~/assets/css/main.css'],
-  build: {
-    postcss: [
-      postcssImport(),
-      postcssCustomProperties(),
-      postcssCustomMedia(),
-      postcssNested(),
-      postcssColorHexAlpha(),
-      postcssFixes(),
-      postcssUrl(),
-      autoprefixer(),
-    ]
-  },
+  build: { postcss },
   modules: [
     '@nuxtjs/axios',
-    '~~/modules/typescript.js'
+    '~~/modules/typescript.js',
   ],
   axios: {}
 };
